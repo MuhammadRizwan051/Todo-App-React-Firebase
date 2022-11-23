8ea0faf398edde00ec5d76a01cc4a43efa911c30
MuhammadRizwan051 <memonrizwan051@gmail.com>
2022-11-23 22:13:53 +0500

    Add Bootstrap and MUI in Todos

--- src/screen/todos.js
+++ src/screen/todos.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import Button from "../components/Button.js";
 import SMButton from "../components/SMButton.js";
 import SMInput from "../components/SMInput.js";
+import { Container } from "@mui/system";
 
 
 function Todos() {
@@ -24,9 +25,9 @@ function Todos() {
         setTxt('')
     };
 
-    // let save = () => {
-    //     console.log('Todos Add')
-    // }
+    let save = () => {
+        console.log('Todos Add')
+    }
 
     // Edit Todos
     let edit = (e, i) => {
@@ -49,35 +50,56 @@ function Todos() {
         <>
             <Typography variant="h2" align="center" color="error">Todo List</Typography>
 
-            <div className="container py-5 px-2" style={{ height: "100%" }}>
-                <div className="row mx-0">
-                    <SMInput label='Todo' value={txt} type='text' placeholder="Enter Todos" className="ps-3 p-1 fs-5 col-xxl-11 col-md-10" onChange={(e) => { setTxt(e.target.value) }} />
-                    <SMButton onClick={add} className="btn btn-success col-xxl-1 col-md-2 mt-3 mt-md-0" label="Add Todo" />
-                    {list.length === 0 ? <p className="text-danger fs-3 text-center mt-4">No Todos to display</p> :
-                        <p className="text-danger fs-3 text-center mt-4">{`Total: ${list.length}`}</p>}
-                </div>
-
+            <Container className="container py-5 px-2" >
+                <Grid container columnSpacing='8'>
+                    <Grid item md={10} sm={9}>
+                        <SMInput label='Enter Todos' value={txt} type='text' placeholder="Enter Todos" onChange={(e) => { setTxt(e.target.value) }} />
+                    </Grid>
+                    <Grid item md={2} sm={3}>
+                        <SMButton onClick={add} color='success' fullWidth label="Add Todo" />
+                    </Grid>
+                    <Grid item md={12} sm={12}>
+                        {list.length === 0 ? <Typography className="text-center text-danger fs-3 text-center mt-4">No Todos to display</Typography> :
+                            <Typography className="text-danger fs-3 text-center mt-4">{`Total: ${list.length}`}</Typography>}
+                    </Grid>
+                </Grid>
 
                 {list.map((e, i) => (
                     <ul className="px-0" key={i}>
-                        <li className="fs-4 mt-5 row mx-0">
-                            <span className="col-1">{`${i + 1}.`}</span>
-                            <span className="col-sm-7 col-xl-9 col-7">{e}</span>
-                            <Button value="Edit" edit="edit btn btn-sm btn-primary col-sm-2 col-xl-1 col-2" click={() => edit(e, i)} />
-                            <Button value="Delete" delete="delete btn-sm btn btn-warning col-sm-2 col-xl-1 col-2" click={() => deleteFunc(e, i)} />
-                            <hr className="mt-2" />
-                        </li>
+                        <Grid container columnSpacing='8'>
+                            <Grid item md={10} sm={8}>
+                                <li className="fs-4 mt-5 row mx-0">
+                                    <span className="col-1">{`${i + 1}.`}</span>
+                                    <span className="col-sm-7 col-xl-9 col-7">{e}</span>
+                                </li>
+                            </Grid>
+                            <Grid item md={1} sm={2} sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                                <SMButton onClick={() => edit(e, i)} color='primary' label="Edit" size='small' fullWidth />
+                            </Grid>
+                            <Grid item md={1} sm={2} sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                                <SMButton onClick={() => deleteFunc(e, i)} color='warning' label="Delete" size='small' fullWidth />
+                            </Grid>
+                            {/* <Button value="Edit" edit="edit btn btn-sm btn-primary col-sm-2 col-xl-1 col-2" click={() => edit(e, i)} /> */}
+                            {/* <Button value="Delete" delete="delete btn-sm btn btn-warning col-sm-2 col-xl-1 col-2" click={() => deleteFunc(e, i)} /> */}
+                        </Grid>
+                        <hr className="mt-2" />
                     </ul>
                 ))}
-                {list.length > 0 ?
-                    (
-                        <>
-                            {/* <Button click={save} value="Save" /> */}
-                            <SMButton onClick={deleteAll} className="btn btn-danger px-3 fs-5 w-50 mt-5" label="Delete All" />
-                        </>
-                    )
-                    : ""}
-            </div>
+                <Grid container>
+                    {list.length > 0 ?
+                        (
+                            <>
+                                <Grid item md={6}>
+                                    <SMButton onClick={save} color='error' fullWidth label="Save" />
+                                </Grid>
+                                <Grid item md={6}>
+                                    <SMButton onClick={deleteAll} color='error' fullWidth label="Delete All" />
+                                </Grid>
+                            </>
+                        )
+                        : ""}
+                </Grid>
+            </Container>
         </>
     )
 }
